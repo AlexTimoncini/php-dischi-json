55867dceb5d1545b99830b877234e40182647ee0
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-07-06 10:09:44 +0200

    minor refactoring on server php

--- server.php
+++ server.php
@@ -1,8 +1,6 @@
 <?php
     $rawData = file_get_contents('./database/dischi.json');
 
-    $decodedData = json_decode($rawData);
-
     header('Content-Type: application/json');
-    echo json_encode($decodedData);
+    echo $rawData;
 ?>
